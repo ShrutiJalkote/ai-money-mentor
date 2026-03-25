5f98e107697a1e2b3ec531d105faba049046f7b0
Shruti Jalkote <132642343+ShrutiJalkote@users.noreply.github.com>
2026-03-25 16:50:04 +0530

    Update Firebase configuration to use environment variables
    Replaced hardcoded Firebase config with environment variables.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,15 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// तुझा config (already correct 👍)
 const firebaseConfig = {
-  apiKey: "AIzaSyCxuoiOa4x8YGmx4lajLaf_-7MMZ-8KFmo",
-  authDomain: "ai-money-mentor-aea2b.firebaseapp.com",
-  projectId: "ai-money-mentor-aea2b",
-  storageBucket: "ai-money-mentor-aea2b.firebasestorage.app",
-  messagingSenderId: "1023598812784",
-  appId: "1:1023598812784:web:fff0a51c291062a2125eb5",
-  measurementId: "G-DJ0MRKKRQY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
